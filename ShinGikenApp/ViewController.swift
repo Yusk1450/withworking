ecb338ec25de1fa9582359c6f5242439b157edc0
山﨑貴史 <yamazakitakashi88@gmail.com>
2026-01-03 20:26:38 +0900

    Initial Commit

---
+++ ShinGikenApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShinGikenApp
+//
+//  Created by 山﨑貴史 on 2026/01/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ShinGikenAppTests/ShinGikenAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShinGikenAppTests.swift
+//  ShinGikenAppTests
+//
+//  Created by 山﨑貴史 on 2026/01/03.
+//
+
+import Testing
+@testable import ShinGikenApp
+
+struct ShinGikenAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
